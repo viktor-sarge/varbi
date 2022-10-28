26d719924837101524bfd1a73edbb4677ccf6190
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-10-28 11:08:44 +0300

    feat(grid): 💄 Add responsiveness to grid
    Gjort gridden responsiv

--- src/components/grid/grid.component.jsx
+++ src/components/grid/grid.component.jsx
@@ -2,7 +2,7 @@ import Card from '../card/card.component';
 
 const Grid = ({ data }) => {
 	return (
-		<div className="container mx-auto grid grid-cols-3 gap-8">
+		<div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 			{data.map((data) => {
 				return <Card data={data} />;
 			})}
