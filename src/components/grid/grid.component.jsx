7cce5cb21a9cf2ca51e2be08e0666c581c6b6246
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-10-28 13:05:52 +0300

    fix(grid): 🐛 add key to cards
    Lagt till en key på korten

--- src/components/grid/grid.component.jsx
+++ src/components/grid/grid.component.jsx
@@ -4,7 +4,7 @@ const Grid = ({ data }) => {
 	return (
 		<div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 			{data.map((data) => {
-				return <Card data={data} />;
+				return <Card key={data.id} data={data} />;
 			})}
 		</div>
 	);
