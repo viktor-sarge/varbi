411b307b9ae53d36ee8bf1840aeff6d8544c1cb1
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-10-28 11:16:30 +0300

    fix(card): 🐛 add word breaks
    Säkerställt att långa ord radbryter och inte bryter designen

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,9 @@ import Label from '../label/label.component';
 const Card = (props) => {
 	return (
 		<div className="rounded-t-lg border p-4 bg-gray-100">
-			<h2 className="text-lg font-bold pb-8">{props.data.title}</h2>
+			<h2 className="text-lg font-bold pb-8 break-words">
+				{props.data.title}
+			</h2>
 			<p>{props.data.town}</p>
 			<div className="flex justify-end">
 				<Label content={props.data.type} background="bg-green-200" />
